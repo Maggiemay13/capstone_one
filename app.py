49acd80770f39d8ce4bdd9f210315fb4ef46264b
maggie <margaret13.may@gmail.com>
2023-11-25 07:07:10 -0600

    connect to server

--- app.py
+++ app.py
@@ -16,11 +16,12 @@ CURR_USER_KEY = "curr_user"
 app = Flask(__name__)
 #
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    "DATABASE_URL", 'postgresql')
+    "DATABASE_URL", 'postgresql:///med-reminder')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = 'any secret string'
 # toolbar = DebugToolbarExtension(app)
 # postgresql:///med-reminder
 
